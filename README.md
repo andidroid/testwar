6f179695a216284b4f4d753bdf6221561dd8ea4b
Andi <andi29392@gmail.com>
2021-10-16 12:01:55 +0200

    added session reource and web clustering

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ mvn dependency:resolve
 
 docker run -e POSTGRES_PASSWORD=postgres -p 5432:15432 postgres:13
 
- mvn flyway:migrate
+mvn flyway:info
+mvn flyway:migrate
+
+
+http://localhost:9990/health
+http://localhost:9990/console/index.html
+
+http://localhost:8080/hello
+http://localhost:8080/test/1
--- src/main/java/me/andidroid/testwar/HelloResource.java
+++ src/main/java/me/andidroid/testwar/HelloResource.java
@@ -2,16 +2,46 @@ package me.andidroid.testwar;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 @Path("/")
 public class HelloResource {
+
+    @Context
+    private HttpServletRequest httpRequest;
+
+    @Inject
+    @ConfigProperty(name = "jboss.node.name")
+    private String node;
 
     @GET
     @Produces("text/plain")
     @Path("/hello")
     public Response hello() {
         return Response.ok("Hello!").build();
+    }
+
+    @GET
+    @Produces("text/plain")
+    @Path("/session")
+    public Response session() {
+        System.out.println("session on node: " + node);
+        System.out.println("session id: " + httpRequest.getSession().getId());
+        Integer count = (Integer) httpRequest.getSession().getAttribute("count");
+        System.out.println("old value of count: " + count);
+        if (count == null) {
+            count = Integer.valueOf(1);
+        } else {
+            count++;
+        }
+        httpRequest.getSession().setAttribute("count", count);
+        return Response.ok(count).build();
     }
 }
