39930b3dfc514c558633d20dd728e362259a633f
Andi <andi29392@gmail.com>
2021-10-27 19:25:50 +0200

    test metrices

--- src/main/java/me/andidroid/testwar/HelloResource.java
+++ src/main/java/me/andidroid/testwar/HelloResource.java
@@ -4,6 +4,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.metrics.annotation.ConcurrentGauge;
+import org.eclipse.microprofile.metrics.annotation.Counted;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
@@ -12,33 +14,41 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
 @Path("/")
-public class HelloResource {
-
+public class HelloResource
+{
+    
     @Context
     private HttpServletRequest httpRequest;
-
+    
     @Inject
     @ConfigProperty(name = "jboss.node.name")
     private String node;
-
+    
     @GET
     @Produces("text/plain")
     @Path("/hello")
-    public Response hello() {
+    @Counted(name = "hello", description = "count of hello method")
+    public Response hello()
+    {
         return Response.ok("Hello!").build();
     }
-
+    
     @GET
     @Produces("text/plain")
     @Path("/session")
-    public Response session() {
+    @Counted(name = "session", description = "count of session method")
+    public Response session()
+    {
         System.out.println("session on node: " + node);
         System.out.println("session id: " + httpRequest.getSession().getId());
         Integer count = (Integer) httpRequest.getSession().getAttribute("count");
         System.out.println("old value of count: " + count);
-        if (count == null) {
+        if(count == null)
+        {
             count = Integer.valueOf(1);
-        } else {
+        }
+        else
+        {
             count++;
         }
         httpRequest.getSession().setAttribute("count", count);
